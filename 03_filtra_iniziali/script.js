22f1d4e5f3e67504d1ec054681aaaf950683789d
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-17 16:09:17 +0100

    filer initial

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,12 +1,16 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const initial = "A";
 
 
 // Dichiara la funzione qui.
-
+function onlyAs(namesAs) {
+    return namesAs.filter(name => name.charAt(0).toUpperCase() === initial.toUpperCase());
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(onlyAs(names));
 
 
 
